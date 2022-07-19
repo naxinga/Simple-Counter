e00e427dcd2e6a9a629f8a64bd291b90470e75cc
naxinga <ngarconf@hotmail.com>
2022-07-19 19:42:51 +0000

    No lo tenia subido aún...

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -17,7 +17,9 @@ let one = setInterval(incSeg,1000);*/
 
 
 const Counter = (props) => {
-    return  <div>{props.segundos}</div>
+    return  <div className="Contador card">
+                <div className="Seis card2">{props.Seis % 10} {props.Cinco % 10} {props.Cuatro % 10} {props.Tres % 10} {props.Dos % 10} {props.Uno % 10}</div>
+            </div>
 	
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,19 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Counter from "./component/counter.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
 
+let Contador = 0;
+setInterval(function(){
+const Seis = Math.floor(Contador/100000);
+const Cinco = Math.floor(Contador/10000);
+const Cuatro = Math.floor(Contador/1000);
+const Tres = Math.floor(Contador/100);
+const Dos = Math.floor(Contador/10);
+const Uno = Math.floor(Contador/1);
+Contador++;
+
+
+ReactDOM.render(<Counter Uno={Uno} Dos={Dos} Tres={Tres} Cuatro={Cuatro} Cinco={Cinco} Seis={Seis}/>, document.querySelector("#app"));
+}, 1000);
